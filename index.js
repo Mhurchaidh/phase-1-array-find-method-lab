8d1654080bfaac40e42006a75a15754fc9d6c05a
mhurchaidh <bmurchie1997@gmail.com>
2023-01-14 20:30:17 -0800

    commit tree d679ab8cda7dfaba816b2159ed083f80493db092

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(record) {
+    const wonGame = record.find(rec => rec.result === "W");
+    if(wonGame){
+        return wonGame.year;
+     }
+     return;
+}
